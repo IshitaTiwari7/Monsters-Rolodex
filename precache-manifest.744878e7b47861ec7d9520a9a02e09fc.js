09b554c62ea7b253313107d6ef7b7daa6976c557
IshitaTiwari227 <Ishita_tiwari@condenast.com>
2020-09-13 13:07:16 +0530

    Updates

---
+++ precache-manifest.744878e7b47861ec7d9520a9a02e09fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554dfd617efaebc975e83efcf7453ac6",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "3e7837123cb6cf8c1fba",
+    "url": "/Monsters-Rolodex/static/css/main.25d8c4a7.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/Monsters-Rolodex/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e7837123cb6cf8c1fba",
+    "url": "/Monsters-Rolodex/static/js/main.f1c952c9.chunk.js"
+  },
+  {
+    "revision": "472349fc95d6c5bdaa6c",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.d0aff6cf.js"
+  }
+]);
